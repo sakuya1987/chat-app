e7e49cdd08a1a46f1a64d19f5bc004abcf7402d6
sakuya1987 <sakuya1987.0713@icloud.com>
2023-11-28 15:59:54 +0900

    Active Storageの導入

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -22,4 +22,10 @@ class MessagesController < ApplicationController
    def message_params
      params.require(:message).permit(:content).merge(user_id: current_user.id)
    end
+
+   private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
  end
